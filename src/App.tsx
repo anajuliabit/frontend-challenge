850493f8db3dcad123e058b0f1a01f9a1deb7cfa
Ana Julia Bittencourt <anajuliabit@gmail.com>
2020-06-09 17:43:15 -0300

    :package: chore: add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-import './App.css';
 import Routes from './routes';
 
-function App() {
-  return <Routes />;
-}
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <Routes />
+  </>
+);
 
 export default App;
