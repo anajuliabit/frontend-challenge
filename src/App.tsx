648dda2064063673d2422ab0ce4c2b440979b40a
Ana Julia Bittencourt <anajuliabit@gmail.com>
2020-06-09 17:28:00 -0300

    :sparkles: feat: implementing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+
 import './App.css';
+import Routes from './routes';
 
 function App() {
-  return <div className="App" />;
+  return <Routes />;
 }
 
 export default App;
